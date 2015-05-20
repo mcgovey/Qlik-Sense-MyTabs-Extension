ee2089220c4657932cbb98fa78ef369c9a9c5b74
Kevin McGovern <kmcg82004@gmail.com>
2015-05-20 09:15:37 -0400

    Working click interaction

--- MyTabs/MyTabs.js
+++ MyTabs/MyTabs.js
@@ -7,6 +7,67 @@ define(["jquery",
 		return '<button class="qirby-button" style="font-size:13px;" data-cmd="' + cmd + '">' + text + '</button>';
 	}
 
+	function render ( $elem, layout ) {
+			//create the app button group
+			var html = '', app = qlik.currApp();
+			//get app location and path
+		    var appName = encodeURIComponent(app.id);
+          	var http = location.protocol;
+          	var slashes = http.concat("//");
+//          	console.log(location.protocol + "//" + window.location.hostname);
+          	var hostname = window.location.hostname; //$element[0].ownerDocument.URL;
+          	var urlPath = "";
+          	if(hostname==="localhost")
+            {
+              urlPath = slashes + hostname + ":4848/sense/app/" + appName + "/sheet/";
+            }
+          	else
+            {
+              urlPath = slashes + hostname + "/sense/app/" + appName + "/sheet/";
+            }
+
+            //set css element
+			$elem.css('overflow', 'auto');
+
+
+		    // Chart object width
+		    var width = $elem.width();
+		    // Chart object height
+		    var height = $elem.height();
+		    // Chart object id
+		    var id = "container_" + layout.qInfo.qId;
+		 
+		    // Check to see if the chart element has already been created
+		    if (document.getElementById(id)) {
+		        // if it has been created, empty it's contents so we can redraw it
+		        $("#" + id).empty();
+		    }
+		    else {
+		        // if it hasn't been created, create it with the appropiate id and size
+		        $elem.append($('<div />;').attr("id", id).width(width).height(height));
+		    }
+			
+
+			html += '<div id="allTabs">';
+			//get list of tab objects and insert into div
+		  	app.getAppObjectList( 'sheet', function(reply){
+
+				$.each(reply.qAppObjectList.qItems, function(key, value) {
+					html += '<div class="singleTab">';
+					html +=  '<a href="' + urlPath + value.qInfo.qId + '/state/analysis">' + value.qData.title + '</a></li> ';
+					html += '</div>';
+				});
+				console.log('inner:'+html);
+
+				$("#" + id).html(html);
+
+			});
+
+			console.log(Object.getOwnPropertyNames(app));
+				 // console.log('outerfunc:'+tab);
+				 // console.log('outer:'+html);
+	}
+
 	return {
 		initialProperties : {
 			version : 1.0,
@@ -42,43 +103,11 @@ define(["jquery",
 				}
 			}
 		},
-		paint : function($element, layout) {
-			$element.css('overflow', 'auto');
-			//create the app button group
-			var html = '', app = qlik.currApp();
-			html += '<div class="qirby-buttongroup">';
-			
-			//Create button for show tabs
-			if(layout.buttons.tabs) {
-				html += createBtn("tabs", "Show Tabs");
-			}
-			
-			//Write to html
-			$element.html(html);
-			//
-			var $fields = $element.find("#fields"), $bookmarks = $element.find("#bookmark_list");
-
-
-
-
-			$element.find('button').on('qv-activate', function() {
-				switch($(this).data('cmd')) {
-					//Alert tab names
-					case 'tabs':
-					  	app.getAppObjectList( 'sheet', function(reply){
-							var str = "";
-							$.each(reply.qAppObjectList.qItems, function(key, value) {
-								str +=  value.qData.title + ' ';
-								// $.each(value.qData.cells, function(k,v){
-								// 	str +=  v.name + ' ';
-								// });
-							});
-							alert(str);
-						});
-						break;
-				}
-			});
-
+		resize: function ( $element, layout ) {
+			render( $element, layout );
+		},
+		paint: function ( $element, layout ) {
+			render( $element, layout );
 		}
 	};
 });
