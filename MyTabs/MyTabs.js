745cbda833c5f613bad9801818ddc488b3cdca22
Kevin McGovern <kmcg82004@gmail.com>
2015-12-16 16:28:45 -0500

    Revert "angular mess"
    This reverts commit e3ddaf474d0ef483136738ed9f78bcbc90a75ee3.

--- MyTabs/MyTabs.js
+++ MyTabs/MyTabs.js
@@ -1,161 +1,133 @@
-define( [
-        './properties',
-        './initialproperties',
-        'text!./template.ng.html',
-        'qlik'
-	],
-	function ( props, initProps, ngTemplate, qlik ) {
-	    'use strict';
+/*global define */
+define([
+	'./js/properties',
+	//Jquery used to get image for each tab $('img.item-thumb-img')
+	"jquery",
+	//mashup and extension interface
+	"qlik",
+	//add stylesheet
+	"css!./style.css",
+	//load bootstrap files
+	"css!./css/scoped-twbs.min.css",
+	"client.utils/state",
+	"client.utils/routing",
+	"./js/bootstrap.min"
+	], 
+	function (props, $, qlik, cssContent, bsCssContent, clientState, clientRedirect) {
+		//apply scoped Bootstrap CSS to header
+		//$( "<style>" ).html( cssContent ).appendTo( "head" );
+		//apply scoped Bootstrap CSS to header
+		//$( "<style>" ).html( bsCssContent ).appendTo( "head" );
 
-	    return {
-	        definition: props,
-	        initialProperties: initProps,
-	        snapshot: {canTakeSnapshot: true},
-	        template: ngTemplate,
-	        controller: ['$scope', function ( $scope ) {
-	            $scope.myTitle = 'This is my AngularJS table';
-	            console.log('layout', $scope.layout);
-	        }]
-	    };
-} );
+		function redirect (sheetId){
+	    	clientRedirect.goToSheet(sheetId, Object.keys(clientState.States)[clientState.state])
+		}
 
-// /*global define */
-// define([
-// 	'./js/properties',
-// 	//Jquery used to get image for each tab $('img.item-thumb-img')
-// 	"jquery",
-// 	//mashup and extension interface
-// 	"qlik",
-// 	//add stylesheet
-// 	"css!./style.css",
-//     'text!./template.ng.html',
-// 	//load bootstrap files
-// 	"css!./css/scoped-twbs.min.css",
-// 	// "client.utils/state",
-// 	// "client.utils/routing",
-// 	"./js/bootstrap.min"
-// 	], 
-// 	function (props, $, qlik, cssContent, ngTemplate, bsCssContent) {//, clientState, clientRedirect
-// 		//apply scoped Bootstrap CSS to header
-// 		//$( "<style>" ).html( cssContent ).appendTo( "head" );
-// 		//apply scoped Bootstrap CSS to header
-// 		//$( "<style>" ).html( bsCssContent ).appendTo( "head" );
+		function render ( $elem, layout) {
+			//create the app button group
+			var html = '', app = qlik.currApp();
 
-// 		// function redirect (sheetId){
-// 	 //    	clientRedirect.goToSheet(sheetId, Object.keys(clientState.States)[clientState.state])
-// 		// }
+            //set css element
+			$elem.css('overflow', 'auto');
 
-// 		// function render ( $elem, layout) {
-// 		// 	//create the app button group
-// 		// 	var html = '', app = qlik.currApp();
+		    // Chart object width
+		    var width = $elem.width();
+		    // Chart object height
+		    var height = $elem.height();
+		    // Chart object id
+		    var id = "container_" + layout.qInfo.qId;
 
-//   //           //set css element
-// 		// 	$elem.css('overflow', 'auto');
+			$elem.empty();
 
-// 		//     // Chart object width
-// 		//     var width = $elem.width();
-// 		//     // Chart object height
-// 		//     var height = $elem.height();
-// 		//     // Chart object id
-// 		//     var id = "container_" + layout.qInfo.qId;
+			html += '<div id="navbar" class="navbar-collapse collapse twbs ' + id + '">';
 
-// 		// 	$elem.empty();
+			//create a list box of navigation items
+			html += '<ul class="nav nav-tabs">';//nav-justified
+			//get list of tab objects and insert into div
+		  	app.getAppObjectList( 'sheet', function(reply){
 
-// 		// 	html += '<div id="navbar" class="navbar-collapse collapse twbs ' + id + '">';
+		  		//for each sheet in the workbook, create a list item
+				$.each(reply.qAppObjectList.qItems, function(key, value) {
 
-// 		// 	//create a list box of navigation items
-// 		// 	html += '<ul class="nav nav-tabs">';//nav-justified
-// 		// 	//get list of tab objects and insert into div
-// 		//   	app.getAppObjectList( 'sheet', function(reply){
+					//include the sheet id as the list item id to be used as a reference for active sheet
+					html += '<li id="' + value.qInfo.qId + '">';// onClick="redirect(' + value.qInfo.qId + ');
 
-// 		//   		//for each sheet in the workbook, create a list item
-// 		// 		$.each(reply.qAppObjectList.qItems, function(key, value) {
+					//wrap anchor tag to be used by bootstrap styling
+					html += '<a>';
 
-// 		// 			//include the sheet id as the list item id to be used as a reference for active sheet
-// 		// 			html += '<li id="' + value.qInfo.qId + '">';// onClick="redirect(' + value.qInfo.qId + ');
-
-// 		// 			//wrap anchor tag to be used by bootstrap styling
-// 		// 			html += '<a>';
-
-// 		// 			//give the link the same name as the sheet
-// 		// 			html += value.qData.title;
+					//give the link the same name as the sheet
+					html += value.qData.title;
 					
-// 		// 			html += '</a>';
-// 		// 			html += '</li>';
+					html += '</a>';
+					html += '</li>';
 
-// 		// 		});
+				});
 
-// 		// 		html += '</ul></div>';
+				html += '</ul></div>';
 
-// 		// 		//insert html into the extension object
-// 		// 		$elem.html(html);
+				//insert html into the extension object
+				$elem.html(html);
 
-// 		// 		//for each sheet in the workbook, attach a click event to point to a different sheet
-// 		// 		$.each(reply.qAppObjectList.qItems, function(key, value) {
-// 		// 			$('div.' + id + ' ul.nav li#'+ value.qInfo.qId).click(function(){
-// 		// 				//clientRedirect.goToSheet(value.qInfo.qId);
-// 		// 				redirect(value.qInfo.qId);
-// 		// 			});
-// 		// 		});
+				//for each sheet in the workbook, attach a click event to point to a different sheet
+				$.each(reply.qAppObjectList.qItems, function(key, value) {
+					$('div.' + id + ' ul.nav li#'+ value.qInfo.qId).click(function(){
+						//clientRedirect.goToSheet(value.qInfo.qId);
+						redirect(value.qInfo.qId);
+					});
+				});
 
-// 		// 		// ------Method for getting active sheet ID using cache-----
+				// ------Method for getting active sheet ID using cache-----
 
-// 		// 		// //find the location that the current tab is stored
-// 		// 		// var cacheName = app.model.session.cacheName;
+				// //find the location that the current tab is stored
+				// var cacheName = app.model.session.cacheName;
 
-// 		// 		// if (cacheName.toLowerCase().indexOf("sheet/") >= 0){
-// 		// 		// 	// Add six to position of cacheName to offset the 'sheet/' index from above
-// 		// 		// 	var cacheNameSheetLoc = cacheName.toLowerCase().indexOf("sheet/")+6;
-// 		// 		// 	// find the end position of the sheet
-// 		// 		// 	var cacheNameSheetLocStop = cacheName.indexOf("/",cacheNameSheetLoc);
-// 		// 		// 	//get the active sheet ID
-// 		// 		// 	var activeSheetID = cacheName.substr(cacheNameSheetLoc,cacheNameSheetLocStop-cacheNameSheetLoc);
-// 		// 		// }
+				// if (cacheName.toLowerCase().indexOf("sheet/") >= 0){
+				// 	// Add six to position of cacheName to offset the 'sheet/' index from above
+				// 	var cacheNameSheetLoc = cacheName.toLowerCase().indexOf("sheet/")+6;
+				// 	// find the end position of the sheet
+				// 	var cacheNameSheetLocStop = cacheName.indexOf("/",cacheNameSheetLoc);
+				// 	//get the active sheet ID
+				// 	var activeSheetID = cacheName.substr(cacheNameSheetLoc,cacheNameSheetLocStop-cacheNameSheetLoc);
+				// }
 
-// 		// 		//get the active sheet ID
-// 		// 		var activeSheetID = clientState.getModel().id;
+				//get the active sheet ID
+				var activeSheetID = clientState.getModel().id;
 
-// 		// 		//set the attribute for the list box of the active sheet to active
-// 		// 		$('#' + activeSheetID).attr('class','active');
+				//set the attribute for the list box of the active sheet to active
+				$('#' + activeSheetID).attr('class','active');
 
-// 		// 		//Toggle Tab Justification	
-// 		// 		if(layout.buttons.justified) {
-// 		// 			$('div.' + id + ' ul.nav').toggleClass("nav-justified");
-// 		// 		}
-// 		// 		//Toggle navbar theme to inverted color scheme
-// 		// 		if(layout.buttons.colored){
-// 		// 			$('div.' + id + ' ul.nav').toggleClass("navbar-inverse");
-// 		// 		}
+				//Toggle Tab Justification	
+				if(layout.buttons.justified) {
+					$('div.' + id + ' ul.nav').toggleClass("nav-justified");
+				}
+				//Toggle navbar theme to inverted color scheme
+				if(layout.buttons.colored){
+					$('div.' + id + ' ul.nav').toggleClass("navbar-inverse");
+				}
 
-// 		// 	});
+			});
 
-// 		// }
+		}
 
-// 		return {
-//             definition: props,
-//             initialProperties: {
-// 				version : 1.0,
-// 				qBookmarkListDef : {
-// 					qType : "bookmark",
-// 					qData : {
-// 						title : "/title",
-// 						description : "/description"
-// 					}
-// 				},
-// 				qFieldListDef : {
-// 				}
-// 			},
-//             snapshot: {canTakeSnapshot: true},
-//             template: ngTemplate,
-//             controller: ['$scope', function ( $scope ) {
-//                 $scope.myTitle = 'This is my AngularJS table';
-//             }]
-//         };
-// 		// resize: function ( $element, layout ) {
-// 		// 	render( $element, layout );
-// 		// },
-// 		// paint: function ( $element, layout) {
-// 		// 	render( $element, layout);
-// 		// }
-// 		// };
-// });
+		return {
+		definition: props,
+		initialProperties : {
+			version : 1.0,
+			qBookmarkListDef : {
+				qType : "bookmark",
+				qData : {
+					title : "/title",
+					description : "/description"
+				}
+			},
+			qFieldListDef : {
+			}
+		},
+		resize: function ( $element, layout ) {
+			render( $element, layout );
+		},
+		paint: function ( $element, layout) {
+			render( $element, layout);
+		}
+	};
+});
